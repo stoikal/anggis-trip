77c9cdaafd5bd176ec35b29e858031b26bb59f81
Laksito Pamilih <laksito@protonmail.com>
2024-11-14 19:45:12 +0700

    phrases screen

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -7,8 +7,11 @@ export default function TabLayout() {
       <Tabs.Screen
         name="(agenda)"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+      <Tabs.Screen
+        name="phrases"
+        options={{
+          title: 'Phrases',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="captions.bubble" color={color} />,
         }}
       />
       <Tabs.Screen
---
+++ app/(tabs)/phrases.tsx
@@ -0,0 +1,20 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function DetailsScreen() {
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text>Konnichiwa</Text>
+      </View>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
