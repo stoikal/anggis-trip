850e9fbcb44b5a99a725801b6424aaab9783e139
Laksito Pamilih <laksito@protonmail.com>
2024-11-15 13:36:49 +0700

    show status bar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,20 @@
 import { Stack } from 'expo-router';
+import {
+  StatusBar,
+} from 'react-native';
 
 export default function RootLayout() {
   return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-      }}
-    >
-      <Stack.Screen name="(tabs)" />
-      <Stack.Screen name="calendar" />
-    </Stack>
+    <>
+      <StatusBar barStyle="dark-content" />
+      <Stack
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="(tabs)" />
+        <Stack.Screen name="calendar" />
+      </Stack>
+    </>
   );
 }
