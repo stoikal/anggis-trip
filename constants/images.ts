1d194184568245a762be740ea3ab82dc49baefbd
Laksito Pamilih <laksito@protonmail.com>
2024-11-19 12:34:40 +0700

    pictures

--- constants/images.ts
+++ constants/images.ts
@@ -1,8 +1,27 @@
 const IMAGES = {
   JAKARTA_1: require("@/assets/images/cover/marcel-ardivan-M2XADSBOWYo-unsplash.jpg"),
   JAKARTA_2: require("@/assets/images/cover/reza-badar-RtDNzjhr-6A-unsplash.jpg"),
+  JAKARTA_3: require("@/assets/images/cover/fuad-najib-dj8qNc8JdBM-unsplash.jpg"),
+  
   TOKYO_1: require("@/assets/images/cover/jaison-lin-2WHTac8jVA8-unsplash.jpg"),
   TOKYO_2: require("@/assets/images/cover/meric-dagli-7NBO76G5JsE-unsplash.jpg"),
+  TOKYO_3: require("@/assets/images/cover/denys-nevozhai--F3wMFrZ7z0-unsplash.jpg"),
+
+  KYOTO_1: require("@/assets/images/cover/mario-hafliger-jSFviCWnUsk-unsplash.jpg"),
+
+  OSAKA_1: require("@/assets/images/cover/kit-ko-9zYjlA4LqTU-unsplash.jpg"),
+  OSAKA_2: require("@/assets/images/cover/denys-nevozhai--F3wMFrZ7z0-unsplash.jpg"),
+
+  FUJI_1: require("@/assets/images/cover/mon-jester-hy9UHdP5yMo-unsplash.jpg"),
+  USJ_1: require("@/assets/images/cover/skaterlunatic-QBO23lMeY5k-unsplash.jpg"),
+
+  
+  JAPAN_1: require("@/assets/images/cover/raphael-lopes-XusVz64-W58-unsplash.jpg"),
+  JAPAN_2: require("@/assets/images/cover/romeo-a-0V5r4umaVWI-unsplash.jpg"),
+
+  HONGKONG_1: require("@/assets/images/cover/aadil-ace-kbCRrh8g9TE-unsplash.jpg"),
+  HONGKONG_2: require("@/assets/images/cover/david-cain-UdrBd-wry50-unsplash.jpg"),
+  HONGKONG_3: require("@/assets/images/cover/meric-dagli-VE3G-xvNcb8-unsplash.jpg"),
 }
 
 export default IMAGES;
--- data/days.ts
+++ data/days.ts
@@ -10,11 +10,39 @@ type DaysMap = {
 }
 
 const DAYS: DaysMap = {
-  "2024-11-15": {
-    coverImage: IMAGES.JAKARTA_2,
+  "2024-11-22": {
+    coverImage: IMAGES.JAKARTA_3,
   },
-  "2024-11-16": {
+
+  "2024-11-23": {
     coverImage: IMAGES.TOKYO_1,
+  },
+  "2024-11-24": {
+    coverImage: IMAGES.FUJI_1,
+  },
+  "2024-11-25": {
+    coverImage: IMAGES.TOKYO_3,
+  },
+  "2024-11-26": {
+    coverImage: IMAGES.TOKYO_2,
+  },
+
+  "2024-11-27": {
+    coverImage: IMAGES.OSAKA_1,
+  },
+  "2024-11-28": {
+    coverImage: IMAGES.USJ_1,
+  },
+  "2024-11-29": {
+    coverImage: IMAGES.KYOTO_1,
+  },
+
+  "2024-11-30": {
+    coverImage: IMAGES.HONGKONG_2,
+  },
+
+  "2024-12-01": {
+    coverImage: IMAGES.JAKARTA_3,
   },
 }
 
